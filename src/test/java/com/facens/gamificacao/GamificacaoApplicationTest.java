ed16c8ef1889a424281aec469621d9e064992aa6
Habila1312 <150145727+Habila1312@users.noreply.github.com>
2025-11-16 22:17:33 -0300

    Create GamificacaoApplicationTest.java

---
+++ src/test/java/com/facens/gamificacao/GamificacaoApplicationTest.java
@@ -0,0 +1,13 @@
+package com.facens.gamificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificacaoApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // Apenas garante que o Spring sobe
+    }
+}
